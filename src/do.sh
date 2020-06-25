046d83f22db446db7950036f10f11fdcd7c1076f
georgevidalakis <georgevidalakis@hotmail.gr>
2020-06-25 19:39:41 +0300

    Enhanced do.sh, implemented git configurators

--- src/do.sh
+++ src/do.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 if [ "$1" != "" ]; then
-    echo "Compiling $1"
+    make
+    echo "Compiling $1..."
     ./pcl < $1 > a.ll || exit 1
     llc-6.0 a.ll -o a.s
     clang a.s lib.a -o a.out
+else
+    echo "ERROR (do.sh): Provide the PCL script name."
 fi
